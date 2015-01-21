e76dfc4115b259c5dfeaafed2936d615001eec5c
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2015-01-21 17:20:08 +0300

    FizzBuzz.

--- solution.js
+++ solution.js
@@ -8,17 +8,13 @@ function drawTriangle() {
   var wall = new Array(len + 1).join(char);
   for (var i = 1; i <= len; ++i) {
     console.log(wall.substr(0, i));
-  }  
+  }
 }
 
 function fizzBuzz() {
-  // Write a program that uses console.log to print all the numbers from 1 to 100,
-  // with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number,
-  // and for numbers divisible by 5 (and not 3), print "Buzz" instead.
-  //
-  // When you have that working, modify your program to print "FizzBuzz",
-  // for numbers that are divisible by both 3 and 5 (and still print "Fizz"
-  // or "Buzz" for numbers divisible by only one of those).
+  for (i = 1; i <= 100; i++) {
+    console.log((i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
+  }
 }
 
 function chessBoard(size) {
