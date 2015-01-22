632a2b3e0cfb534272c40087283ba354560af667
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2015-01-22 15:11:53 +0300

    Chess board complete.

--- solution.js
+++ solution.js
@@ -11,31 +11,19 @@ function drawTriangle() {
 }
 
 function fizzBuzz() {
-  for (i = 1; i <= 100; i++) {
+  for (var i = 1; i <= 100; i++) {
     console.log((i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
   }
 }
 
 function chessBoard(size) {
-  // Write a program that creates a string that represents an 8×8 grid,
-  // using newline characters to separate lines. At each position of the
-  // grid there is either a space or a “#” character. The characters
-  // should form a chess board.
-  //
-  // Passing this string to console.log should show something like this:
-  //
-  //  # # # #
-  // # # # #
-  //  # # # #
-  // # # # #
-  //  # # # #
-  // # # # #
-  //  # # # #
-  // # # # #
-  //
-  // When you have a program that generates this pattern,
-  // define a variable size = 8 and change the program so that
-  // it works for any size, outputting a grid of the given width and height.
+  var line = Array.apply(null, new Array(~~(size/2) + 1)).map(
+    function () { return ' '; }
+  ).join('#');
+
+  return Array.apply(null, new Array(size)).map(
+    function (_, i) { return line.substr(i%2, size); }
+  ).join('\n');
 }
 
 module.exports.squaresOfMax = squaresOfMax;
